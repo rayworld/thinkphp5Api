1b03c33c3239acec0ed744354c2641c21c2c3883
ray <ray@raydeMacBook-Pro.local>
2020-10-04 21:59:45 +0800

    加 timespan token 验证

--- application/api/controller/v1/Common.php
+++ application/api/controller/v1/Common.php
@@ -9,17 +9,48 @@ class Common extends Controller
 {
     protected $request;
 
-    public function _initialize()
+    /**
+     *
+     */
+    public function __construct()
     {
-
-        echo 'init<br/>';
-
-        //parent::_initialize();
-        //$this->request = Request::instance();
+        parent::__construct();
         //$this->check_time($this->request->only(['time']));
+        $this->check_token($this->request->param());
     }
 
-    //
+    /**
+     * Undocumented function
+     *
+     * @param [type] $arr
+     * @return void
+     */
+    public function check_token($arr)
+    {
+        if (!isset($arr['token']) || empty($arr['token'])) {
+            $this->return_msg(402, 'token is empty');
+        }
+
+        $client_token = $arr['token'];
+
+        unset($arr['token']);
+        $server_token = '';
+        foreach ($arr as $key => $Value){
+            $server_token .= $Value;
+        }
+        $server_token=md5("api_".$server_token."_api");
+        //echo $server_token;
+        if ($client_token !== $server_token) {
+            $this->return_msg(403, 'token error');
+        }
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $arr
+     * @return void
+     */
     public function check_time($arr)
     {
         if (!isset($arr['time']) || intval($arr['time'] <= 1)) {
@@ -31,7 +62,14 @@ class Common extends Controller
         }
     }
 
-    //*** */
+    /**
+     * Undocumented function
+     *
+     * @param [int] $code
+     * @param string $msg
+     * @param array $data
+     * @return void
+     */
     public function return_msg($code, $msg = '', $data = [])
     {
         $return_data['code'] = $code;
