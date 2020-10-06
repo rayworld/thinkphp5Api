1fa49e47e0c3185b0da74c2f619b372e32980eb3
ray <ray@raydeMacBook-Pro.local>
2020-10-06 19:43:45 +0800

    发送邮件验证码成功

--- application/api/controller/v1/Captcha.php
+++ application/api/controller/v1/Captcha.php
@@ -1,35 +1,139 @@
 <?php
 
+use PHPMailer\PHPMailer;
+
 namespace app\api\controller\v1;
+
+use PHPMailer\PHPMailer\PHPMailer;
 
 class Captcha extends Common
 {
+    /**
+     * 取得验证码
+     *
+     * @return void
+     */
     public function get_captcha()
     {
+        //取得账户名称
         $account_name = $this->params['account_name'];
+        //信息是否存在标志
         $is_exist = $this->params['is_exist'];
+        //检查账户信息是否正确
         $account_type = $this->check_account_type($account_name);
-        $this->get_captcha_by_account($account_name, $account_type, $is_exist);
+        $this->get_captcha_by_account_type($account_name, $account_type, $is_exist);
     }
 
-    public function get_captcha_by_account($account_name, $account_type, $is_exist)
+    /**
+     * Undocumented function
+     *
+     * @param [type] $account_name
+     * @param [type] $account_type
+     * @param [type] $is_exist
+     * @return void
+     */
+    public function get_captcha_by_account_type($account_name, $account_type, $is_exist)
     {
+        //取得账户类型描述
         $account_type_desc = $account_type == 'mobile' ? '手机' : '邮箱';
-        $this->check_exist($account_name, $account_type, $is_exist);
-        if (session('?' . $account_name . '_last_update_time')) {
-            if (time() - session($account_name . '_last_update_time') < 30) {
-                $this->return_msg(400, $account_type_desc . 'timespan too shot');
+        //检查账户信息是否正确
+        $this->check_user_exist($account_name, $account_type, $is_exist);
+        //上次发送时间不为空
+        if (session('?' . $account_name . '_last_sand_time')) {
+            //判断时间间隔小于30秒
+            if (time() - session($account_name . '_last_sand_time') < 30) {
+                //打印错误信息
+                $this->return_msg(400, $account_type_desc . '验证码请求时间间隔小鱼30秒');
             }
         }
 
+        //生成指定位数的验证码
         $captcha = $this->make_captcha(6);
-        $md5_captcha = md5('account' . md5($captcha));
-        session($account_name . 'captcha', $md5_captcha);
+        $md5_captcha = md5('Account' . '*' . md5($captcha));
+        //保存加密后的验证码
+        session($account_name . '_captcha', $md5_captcha);
+        //保存生成验证码时间
         session($account_name . '_last_update_time', time());
         if ($account_type == 'mobile') {
-            $this->send_captcha_by_mobile($account_name, $account_type, $captcha);
+            //生成手机验证码
+            $this->send_captcha_by_mobile($account_name, $captcha);
         } else {
-            $this->send_captcha_by_email($account_name, $account_type, $captcha);
+            //生成邮箱验证码
+            $this->send_captcha_by_email($account_name, $captcha);
+        }
+    }
+
+
+    /**
+     * 生成指定位数的验证码
+     *
+     * @param [int] $num 生成验证码位数
+     * @return [string] 生成的验证码
+     */
+    public function make_captcha($num)
+    {
+        $max = pow(10, $num) - 1;
+        $min = pow(10, ($num - 1));
+        return rand($min, $max);
+    }
+
+    /**
+     * 生成手机验证码
+     *
+     * @param [string] $account_name 账户名称
+     * @param [string] $captcha 验证码
+     * @return void
+     */
+    public function send_captcha_by_mobile($account_name, $captcha)
+    {
+        echo 'send_captcha_by_mobile';
+    }
+
+    /**
+     * 生成邮箱验证码
+     *
+     * @param [string] $account_name 账户名称
+     * @param [string] $captcha 验证码
+     * @return void
+     */
+    public function send_captcha_by_email($account_name, $captcha)
+    {
+        #echo 'send_captcha_by_email';
+        $mailto = $account_name;
+        $captcha_code = $captcha;
+        $mail = new PHPMailer();
+        //服务器配置
+        $mail->CharSet = "UTF-8";                     //设定邮件编码
+        $mail->SMTPDebug = 0;                        // 调试模式输出
+        $mail->isSMTP();                             // 使用SMTP
+        $mail->Host = 'smtp.sina.com';                // SMTP服务器
+        $mail->SMTPAuth = true;                      // 允许 SMTP 认证
+        $mail->Username = 'casp@sina.com';                // SMTP 用户名  即邮箱的用户名
+        $mail->Password = 'A12345678';             // SMTP 密码  部分邮箱是授权码(例如163邮箱)
+        $mail->SMTPSecure = 'ssl';                    // 允许 TLS 或者ssl协议
+        $mail->Port = 465;                            // 服务器端口 25 或者465 具体要看邮箱服务器支持
+
+        $mail->setFrom('casp@sina.com', 'Ray');  //发件人
+        $mail->addAddress($mailto, 'ray');  // 收件人
+        //$mail->addAddress('ellen@example.com');  // 可添加多个收件人
+        $mail->addReplyTo('casp@sina.com', 'info'); //回复的时候回复给哪个邮箱 建议和发件人一致
+        //$mail->addCC('cc@example.com');                    //抄送
+        //$mail->addBCC('bcc@example.com');                    //密送
+
+        //发送附件
+        // $mail->addAttachment('../xy.zip');         // 添加附件
+        // $mail->addAttachment('../thumb-1.jpg', 'new.jpg');    // 发送附件并且重命名
+
+        //Content
+        $mail->isHTML(false);                                  // 是否以HTML文档格式发送  发送后客户端可直接显示对应HTML内容
+        $mail->Subject = "你有新的验证码";
+        $mail->Body    = "这是一个测试邮件，你的验证码是" . $captcha_code . "，验证码的有效期为一分钟，本邮件请勿回复!";
+        //$mail->AltBody = "邮件客户端不支持HTML则显示此内容";
+
+        if ($mail->send()) {
+            echo 'mail send success!';
+        } else {
+            $this->return_msg(400, $mail->ErrorInfo);
         }
     }
 }
--- application/api/controller/v1/Common.php
+++ application/api/controller/v1/Common.php
@@ -4,13 +4,17 @@ namespace app\api\controller\v1;
 
 use think\Controller;
 use think\Validate;
-
+use think\Db;
 
 class Common extends Controller
 {
+    //请求
     protected $request;
+    //返回的参数
     protected $params;
+    //验证器
     protected $validater;
+    //验证规则
     protected $rules = array(
         'user' => array(
             'login' => array(
@@ -31,108 +35,174 @@ class Common extends Controller
      */
     public function __construct()
     {
-        parent::__construct(); //执行Controller类的构造函数
-        //$this->check_time($this->request->only(['time']));//时间戳验证
-        //$this->check_token($this->request->param());//Token验证
+        //执行Controller类的构造函数
+        parent::__construct();
+        //时间戳验证
+        //$this->check_time($this->request->only(['time']));
+        //Token验证
+        //$this->check_token($this->request->param());
+        //参数过滤
         $this->params = $this->validate_params($this->request->except(['time', 'token']));
     }
 
     /**
-     * Undocumented function
+     * 参数过滤
      *
-     * @param [type] $arr [全部请求参数]
-     * @return void
+     * @param [array] $arr [全部请求参数]
+     * @return 过滤后的参数
      */
     public function validate_params($arr)
     {
+        //取得控制器名，去掉前面的版本号
         $controller_name = substr($this->request->controller(), strpos($this->request->controller(), '.') + 1);
+        //取得操作器名
         $action_name = $this->request->action();
+        //取得验证规则
         $rule = $this->rules[$controller_name][$action_name];
+        //实力话验证器
         $this->validater = new validate($rule);
+        //验证
         if (!$this->validater->check($arr)) {
+            //输出错误信息
             $this->return_msg(404, $this->validater->getError());
         }
+        //返回过滤后的参数
         return $arr;
     }
 
     /**
      * token验证
      *
-     * @param [type] $arr [全部请求参数]
+     * @param [array] $arr [全部请求参数]
      * @return [json] [token 验证结果]
      */
     public function check_token($arr)
     {
+        //token是否存在
         if (!isset($arr['token']) || empty($arr['token'])) {
             $this->return_msg(402, 'token is empty');
         }
-
+        //请求发过来的token
         $client_token = $arr['token'];
-        unset($arr['token']); //去掉token参数
+        //从全部请求参数中去掉token参数
+        unset($arr['token']);
+        //服务器端的token
         $server_token = '';
+        //生成服务器端的token
         foreach ($arr as $key => $Value) {
             $server_token .= $Value;
         }
-        $server_token = md5("api_" . $server_token . "_api");
+        //加密token
+        $server_token = md5("api*" . $server_token . "*api");
+        //对比请求过来的token和服务器token是否一致
         if ($client_token !== $server_token) {
+            //打印错误信息
             $this->return_msg(403, 'token error');
         }
     }
 
     /**
-     * time 验证
+     * 时间戳验证
      *
-     * @param [type] $arr [全部请求参数]
-     * @return [json] [time 验证结果]
+     * @param [array] $arr [全部请求参数]
+     * @return [json] [时间戳验证结果]
      */
     public function check_time($arr)
     {
+        //验证时间戳参数是否存在
         if (!isset($arr['time']) || intval($arr['time'] <= 1)) {
-            $this->return_msg(400, 'timespan error!');
+            $this->return_msg(400, '时间戳不正确!');
         }
-
+        //验证时间戳是否超时
         if (time() - intval($arr['time']) > 60) {
-            $this->return_msg(401, 'time out!');
+            $this->return_msg(401, '验证超时!');
         }
     }
 
     /**
-     * 返回错误信息
+     * 返回错误信息，终止程序继续执行
      *
-     * @param [int] $code
-     * @param string $msg
-     * @param array $data
-     * @return void
+     * @param [int] $code 【错误代码】
+     * @param 【string】 $msg 【错误信息】
+     * @param 【array】 $data 【返回数据】
+     * @return 空
      */
     public function return_msg($code, $msg = '', $data = [])
     {
+        //取得错误代码
         $return_data['code'] = $code;
+        //取得错误信息
         $return_data['msg'] = $msg;
+        //取得返回数据
         $return_data['data'] = $data;
+        //输出错误信息
         echo json_encode($return_data);
+        //终止程序继续执行
         die;
     }
 
     /**
-     * Undocumented function
+     * 取得注册账户类型
      *
-     * @param [type] $account_name
-     * @return void
+     * @param [string] $account_name 注册账户信息
+     * @return 【string】【注册账户类型email或者mobile】
      */
     public function check_account_type($account_name)
     {
+        //验证是不是email
         $is_email = filter_var($account_name, FILTER_VALIDATE_EMAIL) ? 1 : 0;
+        //验证是不是手机
         $is_mobile = strlen($account_name) == 11 && preg_match('/^1[3|4|5|8][0-9]\d{4,8}$/', $account_name) ? 4 : 2;
         $flag = $is_mobile + $is_email;
         switch ($flag) {
             case 2:
-                $this->return_msg(404, 'not mobile or email');
+                $this->return_msg(404, '账户信息不正确');
                 break;
             case 3:
                 return 'email';
                 break;
             case 4:
                 return 'mobile';
+                break;
+        }
+    }
+
+
+    /**
+     * 检查账户信息是否正确
+     *
+     * @param [string] $account_name 账户信息
+     * @param [string] $account_type 账户信息类型：mobile，email
+     * @param [bool] $is_exist 账户信息是否应该存在
+     * @return void
+     */
+    public function check_user_exist($account_name, $account_type, $is_exist)
+    {
+        $type_value = $account_type == 'mobile' ? 2 : 4;
+        $flag = $is_exist + $type_value;
+        $mobile_res = db('account')->where('mobile', $account_name)->find();
+        $email_res = db('account')->where('email', $account_name)->find();
+        switch ($flag) {
+            case 2:
+                if ($mobile_res) {
+                    $this->return_msg(400, '手机号被占用');
+                }
+                break;
+            case 3:
+                if (!$mobile_res) {
+                    $this->return_msg(400, '手机号不存在');
+                }
+                break;
+            case 4:
+                if ($email_res) {
+                    $this->return_msg(400, '邮箱被占用');
+                }
+                break;
+            case 5:
+
+                if (!$email_res) {
+                    $this->return_msg(400, '邮箱不存在');
+                }
                 break;
         }
     }
